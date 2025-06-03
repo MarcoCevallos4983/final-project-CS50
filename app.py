564dcd828beed39873bd28fd55b8fcd8ee8714c5
MarcoCevallos4983 <cevallosmarco4@gmail.com>
2025-06-03 13:17:29 -0500

    Changes in index.html and styl.css

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
 
 app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.route("/")
 def home():
@@ -8,3 +14,4 @@ def home():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
